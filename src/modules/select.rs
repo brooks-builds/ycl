6146e6f3f459215bc373f37c8448867b43a4aeaa
Brookzerker <brooks@brookzerker.com>
2023-05-12 08:37:07 -0600

    adding an ordering option to bboption

--- src/modules/select.rs
+++ src/modules/select.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 use crate::foundations::container::BBContainer;
 use yew::prelude::*;
 
@@ -7,6 +9,8 @@ pub struct Props {
     pub label: AttrValue,
     pub options: Vec<BBOption>,
     pub name: AttrValue,
+    #[prop_or_else(|| "----------".into())]
+    pub select_name: AttrValue,
 }
 
 #[function_component(BBSelect)]
@@ -15,7 +19,7 @@ pub fn component(props: &Props) -> Html {
         <BBContainer>
             <label for={props.id.clone()} class="form-label">{&props.label}</label>
             <select class="form-select" id={props.id.clone()} name={props.name.clone()} data-testid="select">
-                <option value="">{"Choose Tag"}</option>
+                <option value="">{props.select_name.clone()}</option>
                 {
                  props.options.iter().map(|option| html!{ <option value={option.value.clone()}>{option.label.clone()}</option>}).collect::<Html>()
                 }
@@ -28,4 +32,10 @@ pub fn component(props: &Props) -> Html {
 pub struct BBOption {
     pub value: AttrValue,
     pub label: AttrValue,
+}
+
+impl PartialOrd<Self> for BBOption {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        other.value.as_str().partial_cmp(self.value.as_str())
+    }
 }
