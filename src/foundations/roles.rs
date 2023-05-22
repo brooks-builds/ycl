af8e2fc7f969d27706047e35f7cb9acdf196d781
Brookzerker <brooks@brookzerker.com>
2023-05-22 07:37:26 -0600

    fixed warnings

--- src/foundations/roles.rs
+++ src/foundations/roles.rs
@@ -1,5 +1,3 @@
-use std::default;
-
 use serde::{Deserialize, Serialize};
 
 #[derive(PartialEq, Clone, Copy, Eq, Debug, Deserialize, Serialize, Default)]
--- src/modules/select.rs
+++ src/modules/select.rs
@@ -1,5 +1,3 @@
-use std::cmp::Ordering;
-
 use crate::foundations::container::BBContainer;
 use yew::prelude::*;
 
