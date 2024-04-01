b8f101b41c50b165fa822dbd4e5836be383f701d
Brookzerker <brooks@brookzerker.com>
2024-04-01 09:17:59 -0600

    removed the collapsing footer nav, it's always there now

--- src/modules/nav/nav_footer.rs
+++ src/modules/nav/nav_footer.rs
@@ -20,10 +20,7 @@ pub fn component<T: Routable + 'static>(props: &Props<T>) -> Html {
     html! {
         <BBNav aria_label="footer nav">
             <BBContainer margin={BBContainerMargin::Fluid}>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav_footer" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="nav_footer">
+                <div>
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         {
                             props
